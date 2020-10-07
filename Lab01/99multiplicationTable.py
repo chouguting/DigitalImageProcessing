a3bce8a2b2e92f8402ad26e6fe89cf5f9813f14a
chouguting <chouguting@gmail.com>
2020-10-07 15:55:25 +0800

    add Lab2

--- Lab01/99multiplicationTable.py
+++ Lab01/99multiplicationTable.py
@@ -2,9 +2,11 @@
 # coding: utf-8
 
 # In[5]:
-
-
+# i是被乘數
 for i in range(1,10):
+    #j是乘數
     for j in range(1,10):
+        #把所有結果輸出
         print("{} * {} = {}".format(i,j,i*j))
+    #每一部分換行
     print("")
